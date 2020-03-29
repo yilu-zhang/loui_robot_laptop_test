bc1008df2328ff32610858d8a67317be91bb397f
yilu-zhang <861280152@qq.com>
2020-03-29 23:11:26 +0800

    change parameter

--- no_ros/joinMap/joinMap.cpp
+++ no_ros/joinMap/joinMap.cpp
@@ -22,11 +22,11 @@ int main( int argc, char** argv )
         return 1;
     }
     
-    for ( int i=0; i<8; i++ )
+    for ( int i=0; i<1; i++ )
     {
         boost::format fmt( "./%s/%d.%s" ); //图像文件格式
         //colorImgs.push_back( cv::imread( (fmt%"color"%(i+1)%"png").str() ));
-        depthImgs.push_back( cv::imread( (fmt%"depth"%(i+1)%"pgm").str(), -1 )); // 使用-1读取原始图像
+        depthImgs.push_back( cv::imread( (fmt%"depth"%(i+1)%"jpg").str(), -1 )); // 使用-1读取原始图像
         
         double data[7] = {0};
         for ( auto& d:data )
@@ -47,10 +47,10 @@ int main( int argc, char** argv )
     
     // 计算点云并拼接
     // 相机内参 
-    double cx = 325.5;
-    double cy = 253.5;
-    double fx = 518.0;
-    double fy = 519.0;
+    double cx = 321.415982;
+    double cy = 245.98941;
+    double fx = 582.795354;
+    double fy = 584.395006;
     double depthScale = 1000.0;
     
     cout<<"正在将图像转换为点云..."<<endl;
@@ -61,7 +61,7 @@ int main( int argc, char** argv )
     
     // 新建一个点云
     PointCloud::Ptr pointCloud( new PointCloud ); 
-    for ( int i=0; i<8; i++ )
+    for ( int i=0; i<1; i++ )
     {
         cout<<"转换图像中: "<<i+1<<endl; 
         //cv::Mat color = colorImgs[i]; 
